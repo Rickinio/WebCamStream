971d994599aa238aaa4c09a6b2ef363eb68a3621
Ricky Stam <k.stam@hotmail.com>
2018-03-18 15:33:16 +0200

    -working web app with webcam stream...

---
+++ WebCamStream.Web/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OpenCvSharp;
+
+namespace WebCamStream.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static VideoCapture capture;
+
+        static HomeController()
+        {
+            capture = new VideoCapture(0);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetImage()
+        {
+            using (Mat image = new Mat()) // Frame image buffer
+            {
+                capture.Read(image);
+                return File(image.ToBytes(), "image/png");
+            }
+        }
+    }
+}
---
+++ WebCamStream.Web/Startup.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using OpenCvSharp;
+
+namespace WebCamStream.Web
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(options =>
+            {
+                options.ReturnHttpNotAcceptable = false;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            //app.Use(async (context, next) =>
+            //{
+            //    if (context.Request.Path.ToString().Equals("/sse"))
+            //    {
+            //        var response = context.Response;
+            //        response.Headers.Add("Content-Type", "text/event-stream");
+
+            //        VideoCapture capture = new VideoCapture(0);
+
+            //        //int sleepTime = (int)Math.Round(1000 / capture.Fps);
+
+            //        //using (Window window = new Window("capture"))
+            //        //{
+            //        using (Mat image = new Mat()) // Frame image buffer
+            //        {
+            //            // When the movie playback reaches end, Mat.data becomes NULL.
+            //            while (true)
+            //            {
+            //                capture.Read(image); // same as cvQueryFrame
+            //                if (image.Empty())
+            //                    break;
+
+            //                // WriteAsync requires `using Microsoft.AspNetCore.Http`
+            //                var base64img = Convert.ToBase64String(image.ToBytes());
+            //                response.WriteAsync($"data: {base64img}").Wait();
+
+            //                response.Body.Flush();
+            //                await Task.Delay(5 * 1000);
+            //            }
+            //        }
+            //        //}
+
+            //        //for (var i = 0; true; ++i)
+            //        //{
+            //        //    WriteAsync requires `using Microsoft.AspNetCore.Http`
+            //        //    await response
+            //        //        .WriteAsync($"data: Middleware {i} at {DateTime.Now}\r\r");
+
+            //        //    response.Body.Flush();
+            //        //    await Task.Delay(5 * 1000);
+            //        //}
+            //    }
+
+            //    await next.Invoke();
+            //});
+
+            app.UseMvcWithDefaultRoute();
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+    }
+}
